6f61519073779faf369a857d83b1cea49ef015e5
dhruvpuri-goswami <dhruvpuri.j.goswami@gmail.com>
2024-09-03 21:33:17 +0530

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import hello_world, upload_files, upload_success, limit, plant_details, search_plant_by_name, search_plant_by_therapeutic_property
 
 urlpatterns = [
-    path('hello', hello_world, name='hello'),
+    path('hello/', hello_world, name='hello'),
     path('rohantheboss/', upload_files, name='upload_files'),
     path('rohantheboss/success/', upload_success, name='upload_success'),
     path('limit/<int:limit>', limit, name='limit'),
